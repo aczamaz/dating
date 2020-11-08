ae28b0e66671c43cdbd46dd3ab4d342edc77b9cb
aczamaz <aczamaz@gmail.com>
2020-11-08 17:15:06 +0300

    add migration for project

---
+++ database/migrations/2020_11_08_132821_create_messages_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table)
+        {
+            $table->id();
+            $table->timestamps();
+            $table->bigInteger('sender')->unsigned();
+            $table->bigInteger('recipient')->unsigned();
+            $table->string('message');
+        });
+
+        Schema::table('messages', function (Blueprint $table)
+            {
+                $table->foreign('sender')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+
+                $table->foreign('recipient')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
---
+++ database/migrations/2020_11_08_140115_create_vieweds_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateViewedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vieweds', function (Blueprint $table)
+            {
+                $table->id();
+                $table->timestamps();
+                $table->bigInteger('user_a')->unsigned();
+                $table->bigInteger('user_b')->unsigned();
+            }
+        );
+
+        Schema::table('vieweds', function (Blueprint $table)
+            {
+                $table->foreign('user_a')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+
+                $table->foreign('user_b')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+            }git
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vieweds');
+    }
+}
