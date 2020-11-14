3035dea27868e98b21c7e11dbbdb505a98cce753
aczamaz <aczamaz@gmail.com>
2020-11-14 20:06:25 +0300

    add .htaccess and auth controler

---
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\RegistrationRequest;
+
+class RegistrationController extends Controller
+{
+    public function registration(RegistrationRequest $request)
+    {
+        return $request->json();
+    }
+}
